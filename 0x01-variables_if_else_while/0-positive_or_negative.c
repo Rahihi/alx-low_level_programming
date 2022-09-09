2d98b2886b3077616d56fe0bd5ea2109c137b56e
Rahel <rahelalem33@gmail.com>
2022-09-09 00:07:02 -0700

    Task 0 pos or neg

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,6 @@
+#!/bin/bash
+#include <time.h>
+#include <stdlib.h>
+
+srand(time(NULL));   // Initialization, should only be called once.
+int r = rand();      // Returns a pseudo-random integer between 0 and RAND_MAX.1
