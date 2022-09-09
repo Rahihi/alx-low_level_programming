496ad98fdf5697eafc804865fc6a039d90485850
Rahel <rahelalem33@gmail.com>
2022-09-09 14:01:21 -0700

    Task 4 printt alph c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,9 +2,9 @@
 /**
  * main - print if the number is postive, zero, or negative
  *  
- *  Description: using the main function
- *  this program prints "Programming is positive, zero, or negative
- *  Return: 0
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
  */
 int main(void)
 {
@@ -14,7 +14,6 @@ for (ch = 'a' ; ch <= 'z' ; ch++)
 if (ch != 'e' && ch != 'q')
 {
 	putchar(ch);
-}
 }
 putchar('\n');
 return (0);
