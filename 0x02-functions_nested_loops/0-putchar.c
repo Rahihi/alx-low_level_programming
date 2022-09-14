d3925ddb0818d90063ee8a30a3552d3c29ba6af4
Rahel <rahelalem33@gmail.com>
2022-09-14 00:08:59 -0700

    Task0 putchar_

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
-int main()
+#include "main.h"
 
-{     printf("_putcha", '\n');
-	return 0;
+/**
+ * main - print putchar
+ * return 0;
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
 }
