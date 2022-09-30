687ecab39bd17524af49a991f7e6b73645d403b0
Rahel <rahelalem33@gmail.com>
2022-09-30 14:33:40 -0700

    Task2 agrs.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,17 @@
+#include "holberton.h"
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - prints all arguments it receives
- * @argc: number of arguments
- * @argv: array of arguments
- *
+ * main - A program that prints all arguements it receives
+ * @argc: The arguement counter
+ * @argv: The arguement values
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int i;
-
-for (i = 0; i < argc; i++)
+while (argc--)
 {
-printf("%s\n", argv[i]);
+printf("%s\n", *argv++);
+}
+return (0);
 }
