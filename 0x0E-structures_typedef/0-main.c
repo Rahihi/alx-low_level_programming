23eeb27d9193f82541b35c782917eda88b542a69
Rahel <rahelalem33@gmail.com>
2022-10-11 04:53:49 -0700

    task00 dog.hh

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -2,16 +2,17 @@
 #include "dog.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
 {
 struct dog my_dog;
 
-my_dog.name = "Candy";
-my_dog.age = 4.8;
-my_dog.owner = "Meco";
+my_dog.name = "Poppy";
+my_dog.age = 3.5;
+my_dog.owner = "Bob";
 printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 return (0);
 }
